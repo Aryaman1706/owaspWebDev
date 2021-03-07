18c0e334a79b6a71635ecadc441bae86d0de781c
Aryaman1706 <agrover1_be19@thapar.edu>
2021-03-07 12:22:22 +0530

    notSoBasic/array

--- notSoBasic/array.js
+++ notSoBasic/array.js
@@ -58,6 +58,70 @@ for (const value of arr2) {
 
 // * Methords on array
 
-// array.push()
-// array.pop()
-// array.pop()
+const newArr = [1, 2, 3, 4, 5, 6];
+
+// * array.push()
+const newLength = newArr.push(7); // adds an element to the back of array
+console.log(newArr, newLength);
+
+// * array.pop()
+const lastElement = newArr.pop(); // removes the last element in array and returns it
+console.log(newArr, lastElement);
+
+// * array.filter()
+function filterFunction(value) {
+  // this function returns true for values > 0
+  return value > 3;
+}
+const newArr2 = newArr.filter((value, index, array) => filterFunction(value)); // makes a new array of all the elements that passed filterFunction
+console.log(newArr2);
+
+// * array.find()
+function findFunction(value) {
+  return value >= 5;
+}
+const foundElement = newArr.find((value) => findFunction(value)); // returns the first element that passed filter function || undefined
+console.log(foundElement);
+
+// * array.findIndex()
+const indexOfElement = newArr.findIndex((value) => findFunction(value)); // returns the index of first element that passed filter function || -1
+console.log(indexOfElement);
+
+// * array.includes()
+console.log(newArr.includes(5)); // returns true if element is present in array
+
+// * array.map()
+const transformFunction = (value) => {
+  return value ** 2;
+};
+const newSquareArray = newArr.map((value) => transformFunction(value)); // returns a new transformed array
+console.log(newSquareArray);
+
+// * array.slice()
+const shorterArray = newArr.slice(1, 3); // returns a copy of array from start (1st arg) to end (2nd arg)
+console.log(shorterArray);
+
+// * array.sort()
+const compareFunction = (a, b) => {
+  if (a > b) {
+    return -1; // a is sorted before b
+  } else if (a < b) {
+    return 1; // b is sorted before a
+  } else {
+    return 0; // unchanged
+  }
+};
+newArr.sort((a, b) => compareFunction(a, b)); // sort an array according to compare function hence mutates the array
+console.log(newArr);
+
+// * array.reverse()
+newArr.reverse(); // reverse the order of array hence mutates the array
+console.log(newArr);
+
+// * array.splice()
+const start = 1;
+const deleteCount = 2; // optional -> if not provided, all element from start to end will be deleted
+const newElement = 9; // optional -> if not provided, items would be only be removed
+newArr.splice(start, deleteCount, newElement);
+// newArr.splice(start, deleteCount, newElement, 10, 11, 15);
+console.log(newArr);
